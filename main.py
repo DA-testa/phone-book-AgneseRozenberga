f5256c9c731a07917016a24e007bfe5c4558fbc9
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2023-04-02 18:24:37 +0000

    sapratu

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
-        if self.type == 'add':
-            self.name = query[2]
+        if self.type == 'add': self.name = query[2]
 
 def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
+    n = int(input()) ; return [Query(input().split()) for i in range(n)]
 
 def write_responses(result):
     print('\n'.join(result))
