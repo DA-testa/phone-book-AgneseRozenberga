aa04940ff5b8cbfe87e0d1c288c347304d48da6a
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2023-04-02 20:18:18 +0000

    kuku

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 class Query:
     def __init__(self, query):
         self.type, self.number = query[0], int(query[1])
-        if self.type == 'add': self.name = query[2]
+        if self.type == 'add': 
+            self.name = query[2]
 
 def read_queries():
     n = int(input()) ; return [Query(input().split()) for i in range(n)]
 
-def write_responses(result): print('\n'.join(result))
+def write_responses(result): 
+    print('\n'.join(result))
 
 def process_queries(queries):
     result = []
@@ -17,7 +19,8 @@ def process_queries(queries):
     for cur_query in queries:
         if cur_query.type == 'add': contacts[cur_query.number] = cur_query.name
         elif cur_query.type == 'del': 
-            if cur_query.number in contacts: del contacts[cur_query.number]
+            if cur_query.number in contacts: 
+                del contacts[cur_query.number]
         else: response = contacts.get(cur_query.number, 'not found'); result.append(response)
     return result
 
