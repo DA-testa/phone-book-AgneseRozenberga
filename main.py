7e73a850e31035a21ca682d07aead2fdb9394da3
221RDB117 <114361013+AgneseRozenberga@users.noreply.github.com>
2023-04-02 19:28:30 +0000

    .

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ class Query:
     def __init__(self, query):
         self.type, self.number = query[0], int(query[1])
         if self.type == 'add': self.name = query[2]
+
 def read_queries():
     n = int(input()) ; return [Query(input().split()) for i in range(n)]
 
 def write_responses(result): print('\n'.join(result))
 
 def process_queries(queries):
-    result, contacts = [], {}
+    result = [], 
+    contacts = {}
     # Keep list of all existing (i.e. not deleted yet) contacts.
     for cur_query in queries:
         if cur_query.type == 'add': contacts[cur_query.number] = cur_query.name
